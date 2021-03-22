e4f0b465076eb49da00b13f673a043c4c4c1fcc4
ibrahim echikr <ibra3215@icloud.com>
2021-03-21 22:31:24 -0400

    Création des tests unitaires pour la zone probleme

---
+++ src/app/probleme/probleme.component.spec.ts
@@ -0,0 +1,27 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { ProblemeComponent } from './probleme.component';
+
+describe('ProblemeComponent', () => {
+  let component: ProblemeComponent;
+  let fixture: ComponentFixture<ProblemeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [ ProblemeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProblemeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+   it('should create', () => {
+     expect(component).toBeTruthy();
+   });
+});
---
+++ src/app/probleme/probleme.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'inter-probleme',
+  templateUrl: './probleme.component.html',
+  styleUrls: ['./probleme.component.css']
+})
+export class ProblemeComponent implements OnInit {
+  problemeForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.problemeForm = this.fb.group({
+      Probleme: ['',[Validators.minLength(3)]]
+  });
+}
+ 
+save(): void {
+
+}
+
+}
